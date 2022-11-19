98ac472314aea8e96dbbe76edde97a9351becb2b
vali0901 <bobvalentin2003@gmail.com>
2022-11-19 11:02:19 +0200

    Added class with constant integers

---
+++ src/game/MagicNumbers.java
@@ -0,0 +1,24 @@
+package game;
+
+public final class MagicNumbers {
+    private MagicNumbers() { }
+    public static final int MIN_ROW_INDEX = 0;
+    public static final int MAX_ROW_INDEX = 3;
+
+    public static final int ROW_0_INDEX = 0;
+    public static final int ROW_1_INDEX = 1;
+    public static final int ROW_2_INDEX = 2;
+    public static final int ROW_3_INDEX = 3;
+
+    public static final int MAX_ROW_LENGTH = 5;
+
+    public static final int PLAYER_ONE_ID = 1;
+    public static final int PLAYER_TWO_ID = 2;
+
+    public static final int ENVIRONMENT_TYPE = 2;
+    public static final int MINION_TYPE = 1;
+
+    public static final int HERO_MAX_HEALTH = 30;
+
+    public static final int MAX_MANA = 10;
+}
